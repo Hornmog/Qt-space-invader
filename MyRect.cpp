b489d297b15246f55949cea57bb7f258b90b5108
Dmytro <dmytro.maximchuk@gmail.com>
2020-09-21 17:49:26 +0200

    Initial commit, player can shoot bullets

---
+++ MyRect.cpp
@@ -0,0 +1,27 @@
+#include "MyRect.h"
+#include <QGraphicsScene>
+#include <QKeyEvent>
+#include <QDebug>
+#include "Bullet.h"
+
+void MyRect::keyPressEvent(QKeyEvent *event)
+{
+    //qDebug() << "key pressed";
+    if (event->key() == Qt::Key_Left){
+        if(pos().x() > 0){
+            setPos(x()-10,y());
+        }
+    }
+    else if (event->key() == Qt::Key_Right){
+        if(pos().x() + rect().width() < scene()->width()){
+            setPos(x()+10,y());
+        }
+    }
+
+    else if (event->key() == Qt::Key_Space){
+        Bullet * bullet = new Bullet;
+        bullet->setPos(x(),y());
+        scene()->addItem(bullet);
+    }
+
+}
---
+++ main.cpp
@@ -0,0 +1,31 @@
+ #include <QApplication>
+#include <QGraphicsScene>
+#include "MyRect.h"
+#include <QGraphicsView>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    QGraphicsScene * scene = new QGraphicsScene();
+
+    MyRect * player = new MyRect();
+    player->setRect(0,0,100,100);
+    scene->addItem(player);
+
+    player->setFlag(QGraphicsItem::ItemIsFocusable);
+    player->setFocus();
+
+    QGraphicsView * view = new QGraphicsView(scene);
+
+    //???
+    view->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    view->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+    view->show();
+    view->setFixedSize(800,600);
+    scene->setSceneRect(0,0,800,600);
+    //resize window ?
+
+    player->setPos(view->width()/2 - player->rect().width()/2, view->height() - player->rect().height());
+    return a.exec();
+}
