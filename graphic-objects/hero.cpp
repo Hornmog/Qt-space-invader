58c4e5a098ca8b0e0939f901958a606150702e0f
Hornmog <dmytro.maximchuk@gmail.com>
2020-10-19 13:32:13 +0200

    Hero acceleration, WIP

--- graphic-objects/hero.cpp
+++ graphic-objects/hero.cpp
@@ -12,7 +12,12 @@ Hero::Hero(QString imagePath, KeyManager* keyManager) : SpaceShip(nullptr, image
     bulletSpeed = 10;
     xSpeed = 10;     //Speed is a step with which hero moves upon keyPressEvent
     ySpeed = 10;
+    xVelocity = 0;
+    accelNumberOfCycles =  MovementX::accelTime / onTimerPeriod;
+    accelCyclesLeft = accelNumberOfCycles;
+    accel = 0;
     side = Side::hero;
+
 
     //this->setFlag(QGraphicsItem::ItemIsFocusable);
     //this->setFocus();
@@ -26,14 +31,23 @@ Hero::Hero(QString imagePath, KeyManager* keyManager) : SpaceShip(nullptr, image
 
 void Hero::onTimer()
 {
+    groupCheckTextInfo();
+
     if(removalCheck() != Side::nobody){
         heroKilled();
     }
+    else{
+        setPos(x() + calculateXMovement(), y());
+    }
+
 }
 
 void Hero::groupCheckTextInfo()
 {
-
+    QString output = "";
+    output += "Acceleration: " + QString::number(accel) + "\n";
+    output += "Speed: x: " + QString::number(xSpeed) + " y: " + QString::number(ySpeed) + "\n";
+    setCheckText(output);
 }
 
 void Hero::heroKeyPressed(int key)
@@ -41,12 +55,14 @@ void Hero::heroKeyPressed(int key)
     //qDebug() << "key pressed";
     if (key == Qt::Key_Left){
         if(pos().x() > 0){
-            setPos(x()-xSpeed,y());
+            accel = -MovementX::accel;
+            //setPos(x()-xSpeed,y());
         }
     }
     else if (key == Qt::Key_Right){
         if(pos().x() + this->boundingRect().width() < scene()->width()){
-            setPos(x()+xSpeed,y());
+            accel = MovementX::accel;
+            //setPos(x()+xSpeed,y());
         }
     }
     else if (key == Qt::Key_Up){
@@ -66,5 +82,31 @@ void Hero::heroKeyPressed(int key)
         //qDebug() << shootAvl;
     }
 
+}
+
+int Hero::calculateXMovement()
+{
+    accelCyclesLeft--;
+
+    if(accelCyclesLeft < 0){
+        accel = 0;
+        accelCyclesLeft = accelNumberOfCycles;
+    }
+
+    float newXVelocity = xVelocity + accel * (float(onTimerPeriod)/1000);
+
+    if (abs(newXVelocity) > MovementX::maxVelocity){
+        newXVelocity = MovementX::maxVelocity * abs(newXVelocity) / newXVelocity;
+    }
+
+    float distanceMoved = 0.5 * (newXVelocity + xVelocity) * (float(onTimerPeriod)/1000);
+
+//    if(newXVelocity != 0){
+//        newXVelocity = newXVelocity - Movement::heroXFriction * (float(onTimerPeriod)/1000);
+
+//    }
+
+    xVelocity = newXVelocity;
+    return distanceMoved;
 }
 
--- graphic-objects/hero.h
+++ graphic-objects/hero.h
@@ -24,6 +24,17 @@ public slots:
 
 private:
     KeyManager* keyManager;
+    int calculateXMovement();
+    float xVelocity;
+    int accelNumberOfCycles;
+    int accelCyclesLeft;
+
+    struct MovementX {
+        static const int maxVelocity = 200;         //pixels per second
+        static const int accel = 1000;
+        static const int friction = 50;
+        static const int accelTime = 250;
+    };
 
 };
 
