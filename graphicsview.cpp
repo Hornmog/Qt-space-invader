8b66da2125ee61f7f1b4ccfb75125c561987e02a
Hornmog <dmytro.maximchuk@gmail.com>
2020-10-28 10:56:19 +0100

    Set init position of game, according to screen dim

--- graphicsview.cpp
+++ graphicsview.cpp
@@ -1,11 +1,17 @@
 #include "graphicsview.h"
+#include <QApplication>
+#include <QDesktopWidget>
+#include <QDebug>
 
 
 GraphicsView::GraphicsView(QGraphicsScene * scene) : QGraphicsView(scene) {
     //TODO: scroll bar policy ?
+    QRect rect = QApplication::desktop()->screenGeometry(this);       // get size of a user's screen to position game window
+    this->setGeometry(rect.width()/2 - scene->width()/2, rect.height()/2 - scene->height()/2, scene->width(), scene->height());
     this->setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     this->setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     this->show();
+
     this->setFixedSize(scene->width(), scene->height());
     //resize window ?
 }
